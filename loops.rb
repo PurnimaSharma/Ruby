337370de8217da5cad7e77a1a4f03209b6b21da8
PurnimaSharma <purnima.sharma@berylsystems.com>
2016-04-11 18:19:51 +0530

    More alternatives for loops

---
+++ loops.rb
@@ -0,0 +1,12 @@
+3.times do puts "hello" end
+
+# Another alternative
+
+3.times {puts "hello"}
+
+#Another form of loop
+3.upto(7) {|i| puts"hello #{i}"}
+
+#Another form of loop
+7.downto(3) {|i| puts"hello #{i}"}
+
