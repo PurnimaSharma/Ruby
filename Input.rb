d65385f24831e4d3bbcde9df7e742ee713079483
sanchit_singhal <sanchit.singhal@berylsystems.com>
2016-04-11 13:30:21 +0530

    Input from User

---
+++ Input.rb
@@ -0,0 +1,20 @@
+# Input a String
+
+puts "Enter name"
+name = gets
+puts name
+
+# Input Integer
+
+puts "Enter age"
+age = gets.to_i
+puts age
+
+# Input Float
+
+puts "Enter area"
+area= gets.to_f
+puts area
+
+
+
