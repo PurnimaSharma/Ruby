b094091a2150512b001ab8679a0cd546425c6df1
PurnimaSharma <purnima.sharma@berylsystems.com>
2016-04-11 13:58:42 +0530

    addition of two no

---
+++ addition.rb
@@ -0,0 +1,11 @@
+def func(first,second)
+  sum=first + second
+  puts sum
+end
+
+puts "Enter First no"
+no_1 = gets.to_i
+puts "Enter second no"
+no_2 = gets.to_i
+puts "The Sum is: "
+func(no_1,no_2)
